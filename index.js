2969e4f53101defc7cf0668e3e6ffc0d8468c37b
hasan38200 <156702518+hasan38200@users.noreply.github.com>
2025-03-17 09:38:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    while(count!=0){
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0}
+}
